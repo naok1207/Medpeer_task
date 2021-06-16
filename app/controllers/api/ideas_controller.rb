4eef6ee8efe2ba086ab69d540a5ae7114290b8d1
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-06-16 22:10:08 +0900

    edit rm category controller

--- app/controllers/api/ideas_controller.rb
+++ app/controllers/api/ideas_controller.rb
@@ -2,6 +2,13 @@
 
 module Api
   class IdeasController < ApplicationController
+    def show
+        ideas = Idea.response_ideas(params[:category_name])
+        render json: { data: ideas }
+      rescue StandardError
+        render json: { status: 404 }
+      end
+
     def create
       category = Category.find_or_create_by!(name: params[:category_name])
       category.ideas.create!(body: params[:body])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
   namespace :api do
-    resource :ideas, only: :create
-    resource :categories, only: %i[show]
+    resource :ideas, only: %i[ show create ]
   end
 end
