19646aa694d3ed38eb8de44961d47290cc1cf9b1
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-06-17 01:45:43 +0900

    be rubocop

--- app/controllers/api/ideas_controller.rb
+++ app/controllers/api/ideas_controller.rb
@@ -3,11 +3,11 @@
 module Api
   class IdeasController < ApplicationController
     def show
-        ideas = Idea.response_ideas(params[:category_name])
-        render json: { data: ideas }
-      rescue StandardError
-        render json: { status: 404 }
-      end
+      ideas = Idea.response_ideas(params[:category_name])
+      render json: { data: ideas }
+    rescue StandardError
+      render json: { status: 404 }
+    end
 
     def create
       category = Category.find_or_create_by!(name: params[:category_name])
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   namespace :api do
-    resource :ideas, only: %i[ show create ]
+    resource :ideas, only: %i[show create]
   end
 end
